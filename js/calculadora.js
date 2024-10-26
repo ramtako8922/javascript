3dc46cf82e4ab7a18622e1be1c1f1311f90ba546
Ricardo <rmc892020@gmail.com>
2024-10-25 19:55:45 -0500

    calculadora terminada

--- js/calculadora.js
+++ js/calculadora.js
@@ -6,4 +6,39 @@ function escribir(numero) {
   pantalla.value = expresion;
 }
 
+function operar(operador){
+  expresion+=operador;
+  pantalla.value=expresion;
+
+}
+
+function limpiar(){
+  expresion="";
+  pantalla.value="";
+
+}
+
+function borrar() {
+  expresion=expresion.slice(0,-1);
+  pantalla.value=expresion;
+  
+}
+
+function calcular(){
+  try {
+
+    pantalla.value=eval(expresion);
+    expresion=pantalla.value;
+
+    
+  } catch (error) {
+    pantalla.value="Error "+error;
+    expresion=""
+    
+  }
+
+}
+  
+
+
 
